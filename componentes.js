6fb29610dd6444c93cfc7e7c80419c3068412125
walit16 <o.zaaj@sidetours.com>
2025-06-24 16:17:07 +0200

    c

--- componentes.js
+++ componentes.js
@@ -1,23 +1,21 @@
-// componentes.js
-
 function header() {
   return `
-      <header>
-        <h1>header</h1>
-        <nav>
-          <a href="/">Inicio</a>
-          <a href="/acerca">Acerca</a>
-        </nav>
-      </header>
-    `;
+    <header class="bg-green-600 text-white p-5 text-center">
+      <h1 class="text-2xl font-bold">header</h1>
+      <nav class="mt-2 space-x-4">
+        <a href="/" class="text-white hover:underline">Inicio</a>
+        <a href="/acerca" class="text-white hover:underline">Acerca</a>
+      </nav>
+    </header>
+  `;
 }
 
 function footer() {
   return `
-      <footer>
-        <p>Footer</p>
-      </footer>
-    `;
+    <footer class="bg-gray-800 text-white p-4 text-center text-sm mt-8">
+      <p>Footer</p>
+    </footer>
+  `;
 }
 
 window.Componentes = {
